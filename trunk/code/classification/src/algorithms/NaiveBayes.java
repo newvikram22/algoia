d265e0da66a3a6d8fa0f9349ca2d0c1f9a6d81d6
yasserkad <yasserkad@61f27218-abcb-3728-8060-4295677cce19>
2010-04-20 18:43:34 +0000

    read data marche bien

--- trunk/code/classification/src/algorithms/NaiveBayes.java
+++ trunk/code/classification/src/algorithms/NaiveBayes.java
@@ -12,10 +12,10 @@ import java.util.ArrayList;
  */
 public class NaiveBayes implements IClassifier {
 	
-	private ArrayList< ArrayList<String> > attributs;
-	private ArrayList< ArrayList<Integer> > nbAttributs;
-	private ArrayList< ArrayList<ArrayList< Integer > > > nbVal;
-	private ArrayList< ArrayList<ArrayList< Integer > > > nbClasse;
+	private ArrayList<ArrayList<String>> attributs;
+	private ArrayList<ArrayList<Integer>> nbAttributs;
+	private ArrayList<ArrayList<ArrayList<Integer>>> nbVal;
+
 	/**
 	 * lit les données et initialise l'algorithme
 	 * @param inputFile le fichier comportant les données
@@ -26,7 +26,6 @@ public class NaiveBayes implements IClassifier {
 		attributs= new ArrayList< ArrayList<String> >();
 		nbAttributs= new ArrayList< ArrayList<Integer> >();
 		nbVal= new ArrayList< ArrayList<ArrayList< Integer > > >();
-		nbClasse= new ArrayList< ArrayList<ArrayList< Integer > > >();
 		
 		BufferedReader ff= new BufferedReader(new FileReader(inputFile));
 		String s=ff.readLine();
@@ -40,19 +39,20 @@ public class NaiveBayes implements IClassifier {
 			ArrayList<Integer> f =new ArrayList<Integer>();
 			f.add(1);
 			nbAttributs.add(f);
-			if(i!=result.length)
+			if(i!=result.length-1)
 			{
 				ArrayList< ArrayList<Integer> > g =new ArrayList< ArrayList<Integer> >();
+				f =new ArrayList<Integer>();
+				f.add(1);
 				g.add(f);
 				nbVal.add(g);
-				nbClasse.add(g);
 			}
-			//attributs.se//get(i);
 		}
-		
+		printResults();
 		
 		
 		//int n=Integer.parseInt(ff.readLine());
+		boolean b =true;
 		while((s=ff.readLine()) != null)
 		{
 			result = s.split(",");
@@ -67,92 +67,71 @@ public class NaiveBayes implements IClassifier {
 					f.add(1);
 					
 					ArrayList< ArrayList<Integer> > g =nbVal.get(i);
-					ArrayList<Integer> h =g.get(0);
-					ArrayList< ArrayList<Integer> > gc =nbClasse.get(i);
-					ArrayList<Integer> hc =g.get(0);
+					ArrayList<Integer> h =new ArrayList<Integer>(g.get(0));
+					ArrayList<Integer> hc =new ArrayList<Integer>(g.get(0));
 					for(int k=0;k<h.size();k++)
 					{
 						h.set(k,0);
 						hc.set(k,0);
 					}
 					g.add(h);
-					gc.add(hc);
-					nbVal.set(i,g);
-					nbClasse.set(i,gc);
-					j=e.size();
+					j=e.size()-1;
 				}
 				else
 				{
-					f.set(j, f.get(j)+1);
+					f.set(j, f.get(j)+1); 
 				}
-				attributs.set(i,e);
-				nbAttributs.set(i,f);
 				
 				e =attributs.get(result.length-1);
-				int m=e.indexOf(result.length-1);
+				int m=e.indexOf(result[result.length-1]);
+				//System.out.println(result.length-1);
 				if(m<0)
 				{
 					f =nbAttributs.get(result.length-1);
 					e.add(result[result.length-1]);
 					f.add(1);
-					attributs.set(result.length-1,e);
-					nbAttributs.set(result.length-1,f);
+					b=false;
 					
 					for(int k=0;k<result.length-1;k++)
 					{
 						ArrayList< ArrayList<Integer> > g =nbVal.get(k);
-						ArrayList< ArrayList<Integer> > gc =nbClasse.get(k);
 						for(int l=0;l<g.size();l++)
 						{
 							ArrayList<Integer> h=g.get(l);
-							ArrayList<Integer> hc=gc.get(l);
 							if(k==i && l==j)
 							{
 								h.add(1);
-								hc.add(1);
-							}
-							else if(k==i)
-							{
-								h.add(0);
-								hc.add(1);
 							}
 							else
 							{
 								h.add(0);
-								hc.add(0);
 							}
-							g.set(l,h);
-							gc.set(l,hc);
 						}
-						nbVal.set(k,g);
-						nbClasse.set(k,gc);
 					}
 				}
 				else
 				{
+					ArrayList<Integer> z =nbAttributs.get(result.length-1);
+					if(b)
+					{
+						z.set(m,z.get(m)+1);
+						b=false;
+					}
 					ArrayList< ArrayList<Integer> > g =nbVal.get(i);
-					ArrayList< ArrayList<Integer> > gc =nbClasse.get(i);
 					for(int l=0;l<g.size();l++)
 					{
 						ArrayList<Integer> h=g.get(l);
-						ArrayList<Integer> hc=gc.get(l);
 						if(l==j)
 						{
 							h.set(m,h.get(m)+1);
-							hc.set(m,hc.get(m)+1);
 						}
-						else
-						{
-							hc.set(m,hc.get(m)+1);
-						}
-						g.set(l,h);
-						gc.set(l,hc);
 					}
-					nbVal.set(i,g);
-					nbClasse.set(i,gc);
 				}
 			}
+			printResults();
+			b=true;
 		}		
+		
 	}
 	
 	/**
@@ -169,7 +148,38 @@ public class NaiveBayes implements IClassifier {
 	 */
 	@Override
 	public void printResults() {
-		// TODO Auto-generated method stub
+		String s="Attribut:\n\n";
+		ArrayList<Integer> a =nbAttributs.get(attributs.size()-1);
+		for(int i=0;i<attributs.size()-1;i++)
+		{
+			ArrayList<String> e =attributs.get(i);
+			ArrayList<Integer> f =nbAttributs.get(i);
+			ArrayList< ArrayList<Integer> > g =nbVal.get(i);
+			for(int j=0;j<e.size();j++)
+			{		
+				s+=e.get(j)+" "+f.get(j)+" ";
+				if(j!=e.size())
+				{
+					ArrayList<Integer> h =g.get(j);
+					s+="( ";
+					for(int k=0;k<h.size();k++)
+					{
+						s+=h.get(k)+"/"+a.get(k)+",";
+					}
+					s+=") - ";
+				}
+			}
+			s+="\n";
+		}
+		ArrayList<String> e =attributs.get(attributs.size()-1);
+		for(int j=0;j<e.size();j++)
+		{		
+			s+=e.get(j)+" "+a.get(j)+" ";
+			s+=" - ";
+		}
+		s+="\n";		
+		
+		System.out.print(s);
 		
 	}
 
